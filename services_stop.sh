a5f9fedc7ef81f16f5a0c38638d2f0d32a4efb9e
Ivan <39523208+IvanLopatin@users.noreply.github.com>
2019-10-21 17:17:54 +0300

    Update services_stop.sh

--- services_stop.sh
+++ services_stop.sh
@@ -3,11 +3,20 @@
 sleep 5s
 echo "DataBase Stoped -- OK"
 
-status=`ps -aux |grep SCREEN |awk '{print $2}'`
+status=`ps -aux |grep [S]CREEN |awk '{print $2}'`
+jvm_status=`ps -aux |grep [l]ogsta |awk '{print $2}'`
 
 for variable in $status
         do
-        kill -9 $status
+        kill -15 $status &> /dev/null
         #echo "ALL Services stop "
+        sleep 5s
+        kill -9 $status &> /dev/null
         done
+echo "JVM stop"
+kill -15 $jvm_status &> /dev/null
+sleep 5s
+kill -9 $jvm_status &> /dev/null
+
 echo "ALL Services STOP -- OK"
+
