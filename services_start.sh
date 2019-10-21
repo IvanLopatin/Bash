245d4a7c2bafad57319336015d20a1974cf65694
Ivan <39523208+IvanLopatin@users.noreply.github.com>
2019-10-21 16:15:56 +0300

    Update services_start.sh

--- services_start.sh
+++ services_start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 /etc/init.d/elasticsearch start
 sleep 5s
-echo " DataBase start -- OK"
-
-screen -dm -S test1 LogServer  /usr/share/logstash/bin/logstash -f /etc/logstash/logstash-my.conf --config.reload.automatic --path.settings /etc/logstash/ &> /dev/null
+echo "DataBase start -- OK"
+screen -dm -S LogCollector /usr/share/filebeat/bin/filebeat -e -c /etc/filebeat/filebeat.yml &> /dev/null
 sleep 5s
-echo "SecPower_LogServer -- OK"
-screen -dm -S test2_LogCollector /usr/share/filebeat/bin/filebeat -e -c /etc/filebeat/filebeat.yml &> /dev/null
+echo "LogCollector -- OK"
+screen -dm -S test /opt/ELK/kibana/kibana-6.6.1-linux-x86_64/bin/kibana &> /dev/null
 sleep 5s
-echo "SecPower_LogCollector -- OK"
-screen -dm -S test3 /opt/ELK/kibana/kibana-6.6.1-linux-x86_64/bin/kibana &> /dev/null
+echo "test -- OK"
+screen -dm -S test2 /usr/share/logstash/bin/logstash -f /etc/logstash/logstash-my.conf --config.reload.automatic --path.settings /etc/logstash/ &> /dev/null
 sleep 5s
-echo "Services -- OK"
+echo "test2_LogServer -- OK"
