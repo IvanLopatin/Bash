797fa117facf5250b006981c43f812ec624870e2
Ivan <39523208+IvanLopatin@users.noreply.github.com>
2019-10-22 14:05:55 +0300

    Update service_status.sh

--- service_status.sh
+++ service_status.sh
@@ -3,25 +3,41 @@
 Logger=`ps aux |grep [Y]OUR_SERVICE1`
 Log_server=`ps aux |grep [Y]OUR_SERVICE2`
 Server=`ps aux |grep [Y]OUR_SERVICE3`
+ELK=`ps -aux |grep [u]sr/share/elasticsearch/modules/`
+JVM=`ps -aux |grep [u]sr/share/logstash/logstash-core`
 
 if [ -z "$Logger" ]
 then
         echo '[Y]OUR_SERVICE1 --  DOWN'
 else
-        echo '[Y]OUR_SERVICE1 -- STARTED'
+        echo '[Y]OUR_SERVICE1 -- RUNNING'
 fi
 
 if [ -z "$Log_server" ]
 then
         echo '[Y]OUR_SERVICE2 -- DOWN'
 else
-        echo '[Y]OUR_SERVICE2 -- STARTED'
+        echo '[Y]OUR_SERVICE2 -- RUNNING'
 fi
 
 if [ -z "$Server" ]
 then
         echo '[Y]OUR_SERVICE3 -- DOWN'
 else
-        echo '[Y]OUR_SERVICE3 -- STARTED'
+        echo '[Y]OUR_SERVICE3 -- RUNNING'
 fi
 
+
+if [ -z "$ELK" ]
+then
+        echo 'DataBase -- DOWN'
+else
+        echo 'DataBase -- RUNNING'
+fi
+
+if [ -z "$JVM" ]
+then
+        echo 'JVM -- DOWN'
+else
+        echo 'JVM -- RUNNING'
+fi
